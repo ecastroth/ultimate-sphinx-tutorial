70db9dc88d6cbe046540a5fe5c354fc63132dc75
Eduardo Castro <ecastroth@pm.me>
2023-11-08 11:57:08 -0300

    fix extension list

--- guide/source/_static/conf.py
+++ guide/source/_static/conf.py
@@ -31,9 +31,9 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'numpydoc',            # NumPy documentation
-    'sphinx.ext.viewcode'  # Link to local code
-    'myst_parser',         # For using Markdown inside reST
+    'numpydoc',             # NumPy documentation
+    'sphinx.ext.viewcode',  # Link to local code
+    'myst_parser',          # For using Markdown inside reST
 ]
 
 # Add any paths that contain templates here, relative to this directory.
--- guide/source/conf.py
+++ guide/source/conf.py
@@ -41,7 +41,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-# Languaje 
+# Language 
 language = "en"
 
 
@@ -76,13 +76,16 @@ myst_heading_anchors = 4
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
+
 html_theme = 'pydata_sphinx_theme' 
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# -- Theme configuration -----------------------------------------------------
 
 # Sidebar configuration
 html_sidebars = {
